8965af2b14373dd196005b01260a62b4700264c0
fmdallas <ocw163@163.com>
2014-08-30 23:08:34 +0800

    reformat the worldAnalysis.py document

--- wordAnalysis.py
+++ wordAnalysis.py
@@ -1,20 +1,19 @@
 
 
 class WordAnalysis:
+    def __init__(self, text):
+        self.text = text
 
-	def __init__(self,text):
-		self.text = text
+    def get_word_segmentation_list(self):
+        pass
 
-	def get_word_segmentation_list(self):
-		pass
+    def get_word_freq_vector(self):
+        pass
 
-	def get_word_freq_vector(self):
-		pass
+    def cal_word_vect_sim(self, wordvect, sim=None):
+        pass
 
-	def cal_word_vect_sim(self,wordvect,sim=None):
-		pass
-
-	def get_abstract_of_text(self):
-		pass
+    def get_abstract_of_text(self):
+        pass
 
 
