7dc84032e8bcc0520ac00d4f5602458cdae749a2
fmdallas <ocw163@163.com>
2014-08-30 22:50:13 +0800

    build word analysis class wordAnalysis.py

---
+++ wordAnalysis.py
@@ -0,0 +1,20 @@
+
+
+class WordAnalysis:
+
+	def __init__(self,text):
+		self.text = text
+
+	def get_word_segmentation_list(self):
+		pass
+
+	def get_word_freq_vector(self):
+		pass
+
+	def cal_word_vect_sim(self,wordvect,sim=None):
+		pass
+
+	def get_abstract_of_text(self):
+		pass
+
+
