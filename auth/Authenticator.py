b8e5ee9905b2de001ea4705217439b8cbeef9f61
fmdallas <ocw163@163.com>
2014-09-30 22:34:30 +0800

    update doc string of Authenticator.py

--- auth/Authenticator.py
+++ auth/Authenticator.py
@@ -24,23 +24,39 @@ class InvalidPassword(AuthException):
 
 
 class User:
+    """
+    Create a new user object. The password
+    will be encrypted before storing.
+    """
     def __init__(self, username, password):
         self.username = username
         self.password = self._encrypt_pw(password)
         self.is_logged_in = False
 
     def _encrypt_pw(self, password):
+        """
+        Encrypt the password with the username
+        and password and return the sha digest
+        """
         hash_string = (self.username + password)
         hash_string = hash_string.encode('utf8')
         return hashlib.sha256(hash_string).hexdigest()
 
     def check_password(self, password):
+        """
+        Return ture if the password is valid for
+        this user, false otherwise
+        """
         encrypted = self._encrypt_pw(password)
         return encrypted == self.password
 
 
 class Authenticator:
     def __init__(self):
+        """
+        Construct an authenticator to manage
+        users logging in and out
+        """
         self.users = {}
 
     def add_user(self, username, password):
